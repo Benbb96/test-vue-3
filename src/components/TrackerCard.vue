5682d1d33f1c6c38b56bdd7b75969c561d75f8a6
Benbb96 <benbb96@gmail.com>
2021-04-24 11:58:06 +0200

    Create TrackerCard and display list in a flexbox

---
+++ src/components/TrackerCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="tracker-card">
+    <span>@{{ tracker.time }} on {{ tracker.date }}</span>
+    <h4>{{ tracker.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tracker: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.tracker-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.tracker-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
